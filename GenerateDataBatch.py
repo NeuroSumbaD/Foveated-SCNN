9f76ed37292a5c1df7eaa82216d9709bc5d79098
SumbaDee <elsrouji.L@outlook.com>
2021-02-05 13:02:29 -0800

    Simple trained model (binary face classifier)

--- GenerateDataBatch.py
+++ GenerateDataBatch.py
@@ -7,31 +7,48 @@ from os.path import join
 
 
 parser = argparse.ArgumentParser(description="Generate a mixed batch of training data")
-parser.add_argument("-N", type=int, help="Number of samples to generate (assumed even split)", default=250)
-parser.add_argument("-P", type=str, help="Purpose of batch (used for directory name)", default="Development")
+parser.add_argument("-N", type=int, help="Number of samples to generate (assumed even split)", default=256)
+parser.add_argument("-T", help="Prepare training and test data", default="False", action="store_true")
+parser.add_argument("--split", type=float, help="training to test split (0 to 1)", default=0.8)
 
 args = parser.parse_args()
+purpose = "Training" if args.T else "Development"
 paths = []
 
 faceCount = int(np.ceil(args.N/2))
-paths.append(Faces.Pull(purpose=args.P, count=faceCount))
+paths.append(Faces.Pull(purpose=purpose, count=faceCount))
 
 objCount = int(np.floor(args.N/2))
-paths.append(Objects.Pull(purpose=args.P, count=objCount))
+paths.append(Objects.Pull(purpose=purpose, count=objCount))
 
 print("Merging data...")
-batchNum = len(glob(join("Dataset", args.P, "batch*")))
+batchNum = len(glob(join("Dataset", purpose, "batch*")))
+#Initialize numpy arrays
 firstPath = paths.pop(0)
 currFile = np.load(firstPath+".npz")
-data = currFile["data"]
-isFace = currFile["isFace"]
-labels = currFile["labels"]
+count = int(np.ceil(len(currFile["data"])*args.split)) if args.T else args.N
+data, isFace, labels = currFile["data"][:count], currFile["isFace"][:count], currFile["labels"][:count]
+testData, testIsFace, testLabels = currFile["data"][count:], currFile["isFace"][count:], currFile["labels"][count:]
+#Merge arrays
 for fileName in paths:
     currFile = np.load(fileName+".npz")
-    data = np.concatenate((data, currFile["data"]), axis=0)
-    isFace = np.concatenate((isFace, currFile["isFace"]), axis=0)
-    labels = np.concatenate((labels, currFile["labels"]), axis=0)
-mergedPath = join("Dataset", args.P, f"batch_{batchNum}-count_{args.N}")
+    count = int(np.ceil(len(currFile["data"])*args.split)) if args.T else args.N
+    data = np.concatenate((data, currFile["data"][:count]), axis=0)
+    isFace = np.concatenate((isFace, currFile["isFace"][:count]), axis=0)
+    labels = np.concatenate((labels, currFile["labels"][:count]), axis=0)
+    
+    if args.T: #skip if no training/test split
+        testData = np.concatenate((testData, currFile["data"][count:]), axis=0)
+        testIsFace = np.concatenate((testIsFace, currFile["isFace"][count:]), axis=0)
+        testLabels = np.concatenate((testLabels, currFile["labels"][count:]), axis=0)
+
+mergedPath = join("Dataset", purpose, f"train_batch_{batchNum}-count_{len(data)}")
 print(f"Saving {mergedPath} to disk...")
 np.savez(mergedPath,  data=data, isFace=isFace, labels=labels)
+
+if args.T: #Skipt if no training/test split
+    testPath = join("Dataset", purpose, f"test_batch_{batchNum}-count_{len(testData)}")
+    print(f"Saving {testPath} to disk...")
+    np.savez(testPath,  data=testData, isFace=testIsFace, labels=testLabels)
+
 print("Done.")
--- training.py
+++ training.py
@@ -1,2 +1,51 @@
 import numpy as np
 import tensorflow as tf
+from glob import glob
+from os.path import join
+
+def CreateLayers(numConv=3, activation='relu', finalLayer=None):
+    layers = []
+    layers.append(tf.keras.Input(shape=(64,64,3)))
+    for index in range(numConv):
+        layers.append(
+            tf.keras.layers.Conv2D(filters=16*0.5**index, kernel_size=(3,3),
+            strides=(2,2), padding="valid", activation=activation)(layers[-1])
+        )
+    if finalLayer != None:
+        layers.append(tf.keras.layers.Flatten()(layers[-1]))
+        layers.append(finalLayer(layers[-1]))
+    return layers
+
+def CreateModel(layers):
+    return tf.keras.Model(inputs=layers[0], outputs=layers[-1])
+
+def LoadData(batchNum=0, directory="Development"):
+    fileList = glob(join("Dataset", directory, "train*"))
+    dataBatch = np.load(fileList[batchNum])
+    return dataBatch["data"], dataBatch["isFace"], dataBatch["labels"]
+
+
+if __name__ == "__main__":
+    data, isFace, labels = LoadData(directory="Training")
+    data = data.astype('float32') / 255
+
+    model = CreateModel(CreateLayers(finalLayer=tf.keras.layers.Dense(1, activation="relu")))
+    model.summary()
+    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+    history = model.fit(data, isFace, epochs=100)
+    with open("classifierStructure.json", "w") as f:
+        f.write(model.to_json())
+    model.save_weights("classifierWeights.h5")
+
+    import matplotlib.pyplot as plt
+    plt.plot(history.history["loss"])
+    plt.title("Model Loss")
+    plt.xlabel("epochs")
+    plt.ylabel("Binary Crossentropy")
+    plt.show()
+
+    plt.plot(history.history["accuracy"])
+    plt.title("Model accuracy")
+    plt.xlabel("epochs")
+    plt.ylabel("Percent correct")
+    plt.show()
